60aee60953a6c4656ac400ce70439c6d537a8bb8
Ian Bytchek <ianbytchek@gmail.com>
2017-02-11 14:17:24 +0400

    Add custom string convertible support for injection dependency naming

--- source/Injection/Injection.swift
+++ source/Injection/Injection.swift
@@ -21,41 +21,60 @@ Must use `@objc` tag and inherit from NSObject to be usable from Objective C ins
         return instance
     }
 
-    // MARK: -
+    // MARK: init
 
     override public required init() {
         super.init()
     }
 
-    // MARK: -
+    // MARK: get
 
-    @objc open func get(name: String) -> AnyObject? {
-        if let dependency: Dependency = self.dependencies[name] {
-            return dependency.resolve() as AnyObject
+    /*
+    Todo: there's a strong temptation to get rid of this, review the rest of the code and decide if it's worth it…
+    */
+    @objc open func get(name: NSString) -> NSObject? {
+        if let dependency: Dependency = self.dependencies[name as String] {
+            return dependency.resolve() as? NSObject
         } else {
             return nil
         }
     }
 
-    open func get<T>(name: String) throws -> T {
+    open func get<Type>(name: String) throws -> Type {
         guard let dependency: Dependency = self.dependencies[name] else {
             throw Error.undefinedDependency(name)
         }
 
-        if let dependency: T = dependency.resolve() as? T {
+        if let dependency: Type = dependency.resolve() as? Type {
             return dependency
         } else {
             throw Error.uncastableType
         }
     }
 
+    open func get<Name:CustomStringConvertible, Type>(name: Name) throws -> Type {
+        return try self.get(name: name.description)
+    }
+
+    // MARK: add
+
     @discardableResult open func add(name: String, dependency: Dependency) -> Self {
         self.dependencies[name] = dependency
         return self
     }
 
+    @discardableResult open func add<Name:CustomStringConvertible>(name: Name, dependency: Dependency) -> Self {
+        return self.add(name: name.description, dependency: dependency)
+    }
+
+    // MARK: define
+
     @discardableResult open func define(name: String, definition: @escaping Dependency.Definition) -> Self {
         return self.add(name: name, dependency: Dependency(definition: definition))
+    }
+
+    @discardableResult open func define<Name:CustomStringConvertible>(name: Name, definition: @escaping Dependency.Definition) -> Self {
+        return self.define(name: name.description, definition: definition)
     }
 }
 
--- source/Test/Injection/Test.Injection.swift
+++ source/Test/Injection/Test.Injection.swift
@@ -6,17 +6,29 @@ open class InjectionTestCase: TestCase
     open func testDependency() {
         let injection: Injection = Injection.instance
 
-        injection.add(name: "foo", dependency: Injection.Dependency(definition: { NSString(string: "Foo") }))
-        injection.define(name: "bar", definition: { NSString(string: "Bar") })
+        injection.add(name: "foo", dependency: Injection.Dependency(definition: { "Foo" }))
+        injection.define(name: Name.bar, definition: { NSString(string: "Bar") })
+        injection.add(name: "baz", dependency: Injection.Dependency(definition: { 10 }))
+        injection.define(name: Name.qux, definition: { NSNumber(value: 10) })
 
-        let bar: AnyObject? = try! injection.get(name: "bar") as NSString
-        let foo: AnyObject? = try! injection.get(name: "foo") as NSString
+        let foo: NSObject? = injection.get(name: NSString(string: "foo"))
+        let bar: AnyObject = try! injection.get(name: Name.bar)
+        let baz: Int = try! injection.get(name: "baz")
+        let qux: NSNumber = try! injection.get(name: Name.qux)
 
         expect(foo).toNot(beNil())
-        expect(foo).to(beIdenticalTo(try! injection.get(name: "foo") as NSString))
+        expect(foo is String).to(beTrue())
 
         expect(bar).toNot(beNil())
-        expect(bar).to(beIdenticalTo(try! injection.get(name: "bar") as NSString))
+        expect(bar).to(beIdenticalTo(try! injection.get(name: "bar") as String))
+
+        expect(baz).toNot(beNil())
+        expect(baz).to(equal(try! injection.get(name: "baz") as Int))
+        expect(baz).to(equal(try! injection.get(name: "qux") as Int))
+
+        expect(qux).toNot(beNil())
+        expect(qux).to(equal(try! injection.get(name: "qux") as NSNumber))
+        expect(qux).to(equal(try! injection.get(name: "baz") as NSNumber))
     }
 
     open func testInstance() {
@@ -35,4 +47,16 @@ private class FooInjection: Injection
 
 private class BarInjection: Injection
 {
+}
+
+private enum Name: String, CustomStringConvertible
+{
+    public var description: String {
+        return self.rawValue
+    }
+
+    case foo
+    case bar
+    case baz
+    case qux
 }
