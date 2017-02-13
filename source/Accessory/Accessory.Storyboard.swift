26b8540b1d7ff0546884f68c471c384bba37666a
Ian Bytchek <ianbytchek@gmail.com>
2017-02-13 12:49:17 +0400

    Add storyboard controller view loading construction parameter

--- source/Accessory/Accessory.Storyboard.swift
+++ source/Accessory/Accessory.Storyboard.swift
@@ -1,4 +1,4 @@
-import AppKit.NSStoryboard
+import AppKit
 
 public protocol StoryboardController
 {
@@ -6,7 +6,17 @@ public protocol StoryboardController
 
 extension StoryboardController
 {
-    public static func construct(from storyboard: NSStoryboard) -> Self {
-        return storyboard.instantiateController(withIdentifier: String(describing: self)) as! Self
+    public static func construct(from storyboard: NSStoryboard, load: Bool = true) -> Self {
+        let controller: Self = storyboard.instantiateController(withIdentifier: String(describing: self)) as! Self
+
+        // In most cases it doesn't matter whether the view is loaded or not, but in rare cases it needs to be loaded 
+        // immediately. Since controller gets used / view gets loaded shortly after instantiation, we can load it here
+        // by default, as it won't have any significant impact.
+
+        if load, let controller: NSViewController = controller as? NSViewController, !controller.isViewLoaded {
+            controller.loadView()
+        }
+
+        return controller
     }
 }
