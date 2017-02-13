bae19e8f744bd73fe42aec1a2a9b69b45539e43b
Ian Bytchek <ianbytchek@gmail.com>
2017-02-13 12:01:44 +0400

    Remove construct method from the storyboard controller protocol

--- source/Accessory/Accessory.Storyboard.swift
+++ source/Accessory/Accessory.Storyboard.swift
@@ -1,15 +1,12 @@
-import AppKit
+import AppKit.NSStoryboard
 
 public protocol StoryboardController
 {
-    associatedtype StoryboardController
-
-    static func construct(from storyboard: NSStoryboard) -> StoryboardController
 }
 
 extension StoryboardController
 {
-    public static func construct(from storyboard: NSStoryboard) -> StoryboardController {
-        return storyboard.instantiateController(withIdentifier: String(describing: self)) as! StoryboardController
+    public static func construct(from storyboard: NSStoryboard) -> Self {
+        return storyboard.instantiateController(withIdentifier: String(describing: self)) as! Self
     }
 }
