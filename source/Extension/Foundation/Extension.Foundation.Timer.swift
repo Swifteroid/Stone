ede39aac5cf84b3228421e2780481408f385d72d
Ian Bytchek <ianbytchek@gmail.com>
2017-11-16 13:12:42 +0400

    Use concrete handler signatures in timer schedule extensions

--- source/Extension/Foundation/Extension.Foundation.Timer.swift
+++ source/Extension/Foundation/Extension.Foundation.Timer.swift
@@ -2,68 +2,46 @@ import Foundation
 
 extension Timer
 {
-    private typealias Block = () -> ()
-    private typealias TimerHandler = (Timer) -> ()
-    private typealias TimerHandlerOptional = (Timer?) -> ()
 
-    /// Creates and schedules a one-time `NSTimer` instance.
-    /// 
-    /// - parameters delay: The delay before execution.
-    /// - parameters handler: A closure to execute after `delay`.
-    /// - returns: The newly-created `NSTimer` instance.
-
-    @discardableResult public static func schedule(delay: TimeInterval, handler: Any) -> Timer {
-        var timerHandler: TimerHandlerOptional!
-
-        if let handler: Block = handler as? Block {
-            timerHandler = { (timer: Timer?) in handler() }
-        } else if let handler: TimerHandler = handler as? TimerHandler {
-            timerHandler = { (timer: Timer?) in handler(timer!) }
-        } else if let handler: TimerHandlerOptional = handler as? TimerHandlerOptional {
-            timerHandler = handler
-        } else {
-            preconditionFailure()
-        }
-
-        if #available(OSX 10.12, *) {
-            return self.scheduledTimer(withTimeInterval: delay, repeats: false, block: timerHandler)
-        } else {
-            let fireDate: TimeInterval = delay + CFAbsoluteTimeGetCurrent()
-            let timer: Timer = CFRunLoopTimerCreateWithHandler(kCFAllocatorDefault, fireDate, 0, 0, 0, timerHandler)
-            CFRunLoopAddTimer(CFRunLoopGetCurrent(), timer, CFRunLoopMode.commonModes)
-            return timer
-        }
-    }
-
-    /// Creates and schedules a repeating `NSTimer` instance.
+    /// Creates and schedules a `Timer` instance.
     ///
+    /// - parameters delay: The delay (in seconds) before execution.
     /// - parameters interval: The interval (in seconds) between each execution of `handler`. Note that
     ///   individual calls may be delayed; subsequent calls to `handler` will be based on the time the 
     ///   timer was created.
-    /// - parameters handler: A closure to execute at each `interval`.
-    /// 
-    /// - Returns: The newly-created `NSTimer` instance.
+    /// - parameters handler: Block to execute at each `interval`.
+    ///
+    /// - returns: Scheduled `Timer` instance.
 
-    @discardableResult public static func schedule(interval: TimeInterval, handler: Any) -> Timer {
-        var timerHandler: TimerHandlerOptional!
+    @discardableResult public static func schedule(delay: TimeInterval, interval: TimeInterval, handler: @escaping (Timer) -> ()) -> Timer {
+        let timer: Timer = CFRunLoopTimerCreateWithHandler(kCFAllocatorDefault, delay + CFAbsoluteTimeGetCurrent(), interval, 0, 0, { handler($0!) })
+        CFRunLoopAddTimer(CFRunLoopGetCurrent(), timer, CFRunLoopMode.commonModes)
+        return timer
+    }
 
-        if let handler: Block = handler as? Block {
-            timerHandler = { (timer: Timer?) in handler() }
-        } else if let handler: TimerHandler = handler as? TimerHandler {
-            timerHandler = { (timer: Timer?) in handler(timer!) }
-        } else if let handler: TimerHandlerOptional = handler as? TimerHandlerOptional {
-            timerHandler = handler
-        } else {
-            preconditionFailure()
-        }
+    @discardableResult public static func schedule(delay: TimeInterval, interval: TimeInterval, handler: @escaping () -> ()) -> Timer {
+        return self.schedule(delay: delay, interval: interval, handler: { _ in handler() })
+    }
+}
 
-        if #available(OSX 10.12, *) {
-            return self.scheduledTimer(withTimeInterval: interval, repeats: true, block: timerHandler)
-        } else {
-            let fireDate: TimeInterval = interval + CFAbsoluteTimeGetCurrent()
-            let timer: Timer = CFRunLoopTimerCreateWithHandler(kCFAllocatorDefault, fireDate, interval, 0, 0, timerHandler)
-            CFRunLoopAddTimer(CFRunLoopGetCurrent(), timer, CFRunLoopMode.commonModes)
-            return timer
-        }
+extension Timer
+{
+    @discardableResult public static func schedule(delay: TimeInterval, handler: @escaping (Timer) -> ()) -> Timer {
+        return self.schedule(delay: delay, interval: 0, handler: handler)
+    }
+
+    @discardableResult public static func schedule(delay: TimeInterval, handler: @escaping () -> ()) -> Timer {
+        return self.schedule(delay: delay, interval: 0, handler: handler)
+    }
+}
+
+extension Timer
+{
+    @discardableResult public static func schedule(interval: TimeInterval, handler: @escaping (Timer) -> ()) -> Timer {
+        return self.schedule(delay: interval, interval: interval, handler: handler)
+    }
+
+    @discardableResult public static func schedule(interval: TimeInterval, handler: @escaping () -> ()) -> Timer {
+        return self.schedule(delay: interval, interval: interval, handler: handler)
     }
 }
