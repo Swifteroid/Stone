33d0018b7d28721bcb92a7f6fc5af8e00e928262
Ian Bytchek <ianbytchek@gmail.com>
2018-03-16 14:04:13 +0400

    Add core graphics infinite size fitting / filling test case

--- source/Test/Extension/Test.Extension.CoreGraphics.Size.swift
+++ source/Test/Extension/Test.Extension.CoreGraphics.Size.swift
@@ -14,5 +14,10 @@ internal class CGSizeExtensionTestCase: TestCase
         expect(size.fitting(aspect: CGSize(width: 200, height: 200))) == CGSize(width: 200, height: 100)
         expect(size.fitting(aspect: CGSize(width: 200, height: 100))) == CGSize(width: 200, height: 100)
         expect(size.fitting(aspect: CGSize(width: 100, height: 200))) == CGSize(width: 100, height: 50)
+
+        expect(size.fitting(aspect: CGSize(width: CGFloat.infinity, height: 200))) == CGSize(width: 400, height: 200)
+        expect(size.fitting(aspect: CGSize(width: 200, height: CGFloat.infinity))) == CGSize(width: 200, height: 100)
+        expect(size.filling(aspect: CGSize(width: CGFloat.infinity, height: 200))) == CGSize.infinite
+        expect(size.filling(aspect: CGSize(width: 200, height: CGFloat.infinity))) == CGSize.infinite
     }
 }
