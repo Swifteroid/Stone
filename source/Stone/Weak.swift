0380e8b8476f0875040e2fc360e08712c9048200
Ian Bytchek <ianbytchek@gmail.com>
2018-06-16 10:58:11 +0400

    Add weak reference class

---
+++ source/Stone/Weak.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+/// Weak object reference wrapper, works great with collections and other structs which need to store weak value
+/// references, but cannot do so directly.
+open class Weak<Reference: AnyObject>
+{
+    public init(_ reference: Reference? = nil) {
+        self.reference = reference
+    }
+
+    open weak var reference: Reference?
+}
