5d67b6ebea72fcd78c06b5d1ee980bdcad8ec930
Ian Bytchek <ianbytchek@gmail.com>
2019-01-07 09:00:19 +0000

    Use device-specific color space configuration in CGImage extensions

--- source/Extension/CoreGraphics/Extension.CGImage.swift
+++ source/Extension/CoreGraphics/Extension.CGImage.swift
@@ -3,13 +3,16 @@ import CoreGraphics
 extension CGImage
 {
 
-    /// Returns current image resized to specified size and quality.
+    /// Returns current image resized to specified size and quality, the new image might have different characteristics.
     public func resize(to size: CGSize, interpolationQuality: CGInterpolationQuality? = nil) -> CGImage {
-        let context: CGContext = CGContext(data: nil, width: Int(round(size.width)), height: Int(round(size.height)), bitsPerComponent: self.bitsPerComponent, bytesPerRow: 0, space: self.colorSpace ?? CGColorSpaceCreateDeviceRGB(), bitmapInfo: self.alphaInfo.rawValue)!
 
+        // The context should be created based on the local system, not the current image, otherwise we might 
+        // end up with `unsupported parameter combination` failures – they are common when loading screenshots 
+        // stored directly by the Screen Capture app.
+
+        let context: CGContext = CGContext(data: nil, width: Int(round(size.width)), height: Int(round(size.height)), bitsPerComponent: 8, bytesPerRow: 0, space: CGColorSpaceCreateDeviceRGB(), bitmapInfo: CGImageAlphaInfo.premultipliedFirst.rawValue)!
         context.interpolationQuality = interpolationQuality ?? CGInterpolationQuality.high
         context.draw(self, in: CGRect(origin: CGPoint.zero, size: size))
-
         return context.makeImage()!
     }
 
