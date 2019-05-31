da6e0b26838fdc0b67e4d68c0ed7f1a172b9c0ab
Ian Bytchek <ianbytchek@gmail.com>
2019-05-31 19:50:59 +0100

    Make weak class final

--- source/Stone/Weak.swift
+++ source/Stone/Weak.swift
@@ -2,10 +2,10 @@ import Foundation
 
 /// Weak object reference wrapper, works great with collections and other structs which need to store weak value
 /// references, but cannot do so directly.
-open class Weak<Reference: AnyObject> {
+final public class Weak<Reference: AnyObject> {
     public init(_ reference: Reference? = nil) {
         self.reference = reference
     }
 
-    open weak var reference: Reference?
+    public weak var reference: Reference?
 }
